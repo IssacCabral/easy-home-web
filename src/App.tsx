9c164bca8f125e4ad318d1a4b1cc5f49e7b2e362
Issac Cabral <clidenorissac@gmail.com>
2024-10-03 14:42:47 -0300

    🎉 init: add shadcn and tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
+import "./index.css";
+import { Button } from "./components/ui/button";
+
 export function App() {
   return (
     <>
       <div>Easy Home Web</div>
+      <Button variant="destructive">Clique on Me</Button>
+      <Button>Clique on Me</Button>
+      <Button>Clique on Me</Button>
     </>
   );
 }
