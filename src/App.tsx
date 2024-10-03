e895fa47b808ea92726cb3356c6f444d74e78c6d
Issac Cabral <clidenorissac@gmail.com>
2024-10-03 14:55:54 -0300

    🐳 chore: change project colors schema

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,9 @@
 import "./index.css";
-import { Button } from "./components/ui/button";
 
 export function App() {
   return (
     <>
-      <div>Easy Home Web</div>
-      <Button variant="destructive">Clique on Me</Button>
-      <Button>Clique on Me</Button>
-      <Button>Clique on Me</Button>
+      <h1>Eae meu amigo</h1>
     </>
   );
 }
