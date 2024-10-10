90e14e16ff1a352ccfefdfaaf20575f421ae3d97
Issac Cabral <clidenorissac@gmail.com>
2024-10-10 05:52:37 -0300

    🐳 chore: add mode toggle

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./routes";
+import { ThemeProvider } from "./components/theme-provider";
 
 export function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <ThemeProvider>
+      <RouterProvider router={router} />;
+    </ThemeProvider>
+  );
 }
