bea56acd47b190f582fcc247389caf37f42c6e02
Issac Cabral <clidenorissac@gmail.com>
2024-10-15 19:51:45 -0300

    🐳 chore: define reValidate form mode

--- src/pages/app/property-list/property-list.tsx
+++ src/pages/app/property-list/property-list.tsx
@@ -39,6 +39,8 @@ export function PropertyList() {
   const form = useForm<FindPropertiesForm>({
     resolver: zodResolver(findPropertiesForm),
     defaultValues,
+    mode: "onSubmit",
+    reValidateMode: "onSubmit",
   });
 
   const { data: result, isFetching } = useQuery({
