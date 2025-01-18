0f78ac9a0357e34ab0af347e2355c6f6bc783073
Issac Cabral <clidenorissac@gmail.com>
2025-01-18 08:46:47 -0300

    ✨ feat: add carousel

--- src/pages/app/property-details/components/details.tsx
+++ src/pages/app/property-details/components/details.tsx
@@ -1,7 +1,7 @@
-import PropertyDetailsImg from "@/assets/property-details.png";
 import { Badge } from "@/components/badge";
 import { LocationInfo } from "@/components/location-info";
 import { PropertyInfoSummary } from "@/components/property-info-summary";
+import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from "@/components/ui/carousel";
 import { IPropertyEntity } from "@/shared/property";
 import { DollarSign } from "lucide-react";
 
@@ -10,10 +10,28 @@ interface DetailsProps {
   rating: number;
 }
 
+const images = [
+  "/src/assets/carousel/carousel-1.jpg",
+  "/src/assets/carousel/carousel-2.jpg",
+  "/src/assets/carousel/carousel-3.jpg",
+  "/src/assets/carousel/carousel-4.jpg",
+  "/src/assets/carousel/carousel-5.jpg",
+];
+
 export function Details(props: DetailsProps) {
   return (
     <section className="flex w-[745px] flex-col gap-3">
-      <img src={PropertyDetailsImg} alt="Property Details" className="w-full" />
+      <Carousel>
+        <CarouselContent>
+          {images.map((img, index) => (
+            <CarouselItem key={index} className="flex h-[464px] w-[745px] items-center justify-center overflow-hidden">
+              <img src={img} alt={`Property Slide ${index + 1}`} className="h-full w-full rounded-xl object-cover" />
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+        <CarouselPrevious />
+        <CarouselNext />
+      </Carousel>
       <div className="flex justify-between">
         <h1 className="text-3xl font-bold text-landing">{props.property.title}</h1>
         <div className="flex gap-5">
